a302155b184958f9d5a67a8bc4dbd9db8a5c39ed
RCrobotcat <136684976+RCrobotcat@users.noreply.github.com>
2024-08-29 14:09:56 +0100

    Fixed bug: Player's Input Direction will be Rotated to Camera's World Space.

--- Assets/Scripts/Characters/playerController.cs
+++ Assets/Scripts/Characters/playerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -61,7 +62,8 @@ public class playerController : MonoBehaviour
         Vector3 inputDirection = new Vector3(horizontal, 0, vertical);
         if (inputDirection != Vector3.zero)
         {
-            MovePlayer(inputDirection);
+            Vector3 CamRelativeMove = ConvertToCameraSpace(inputDirection);
+            MovePlayer(CamRelativeMove);
         }
 
         isDead = characterStatus.currentHealth == 0;
@@ -79,6 +81,24 @@ public class playerController : MonoBehaviour
         Vector3 targetPosition = transform.position + inputDirection;
 
         agent.SetDestination(targetPosition);
+    }
+
+    private Vector3 ConvertToCameraSpace(Vector3 vectorToRotate)
+    {
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+        Vector3 CamForwardZProduct = vectorToRotate.z * camForward;
+        Vector3 CamRightXProduct = vectorToRotate.x * camRight;
+
+        Vector3 vectorRotateToCameraSpace = CamForwardZProduct + CamRightXProduct;
+        return vectorRotateToCameraSpace;
     }
 
     private void switchAnimation()
